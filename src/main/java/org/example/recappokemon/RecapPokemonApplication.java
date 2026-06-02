0c32d5acd39120db61fc6ec0f62ee88a86dc2993
Roland-Hufnagel <roland.hufnagel@gmx.de>
2026-06-02 10:00:06 +0200

    Initial commit

---
+++ src/main/java/org/example/recappokemon/RecapPokemonApplication.java
@@ -0,0 +1,13 @@
+package org.example.recappokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/recappokemon/RecapPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recappokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
